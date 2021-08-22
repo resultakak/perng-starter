de11194f3d646f70917aaf86509c014565115de9
Resul Takak <resultakak@gmail.com>
2021-08-23 01:05:34 +0300

    Update RegisterPage.jsx

--- client/src/components/Pages/RegisterPage.jsx
+++ client/src/components/Pages/RegisterPage.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import GeneralLayout from "../Layouts/GeneralLayout";
-import Container from "../core/Container";
-import Hero from "../core/Hero";
 
 const RegisterPage = () => {
+  const [fields, setFields] = React.useState({});
+  const [completed, setCompleted] = React.useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const changeField = (e) => {
+    setFields({
+      ...fields,
+      [e.target.name]: e.target.value,
+    });
+
+    if (fields.name && fields.surname) {
+      setCompleted(true);
+    }
+  };
+
   return (
       <React.Fragment>
         <GeneralLayout>
@@ -17,6 +33,51 @@ const RegisterPage = () => {
                 <p className="subtitle">
                   Please sign up!
                 </p>
+              </div>
+            </div>
+          </section>
+
+          <section className="section mt-4">
+            <div className="container">
+              <div className="columns">
+                <div className="column is-6 is-offset-3">
+
+                  <form onSubmit={handleSubmit}>
+                    <div className="field">
+                      <label className="label">Name</label>
+                      <div className="control">
+                        <input name="name" type="text" className="input"
+                               onChange={changeField} placeholder="Name" required={true} />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <label className="label">Surname</label>
+                      <div className="control">
+                        <input name="surname" type="text" className="input"
+                               onChange={changeField} placeholder="Surname" required={true} />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <label className="label">E-mail</label>
+                      <div className="control">
+                        <input name="email" type="email" className="input"
+                               onChange={changeField} placeholder="E-mail" required={true} />
+                      </div>
+                    </div>
+                    <div className="field is-grouped">
+                      <div className="control">
+                        <button className="button is-link"
+                                disabled={!completed}>Submit
+                        </button>
+                      </div>
+                      <div className="control">
+                        <button className="button is-link is-light">Cancel
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+
+                </div>
               </div>
             </div>
           </section>
