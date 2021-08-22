9dce06c75b20031c538aa2c38fe7080c64cb0b99
Resul Takak <resultakak@gmail.com>
2021-08-23 00:28:59 +0300

    Update index.js

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,5 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 4000,
+  REDIS_HOST: process.env.REDIS_HOST || 'localhost',
+  REDIS_PORT: process.env.REDIS_PORT || '6379',
   emailMinLength: 8,
   passwordMinLength: 8,
 };
