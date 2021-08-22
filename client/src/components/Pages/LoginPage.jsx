d65771e847eb15c8e1175414951a2b9194367253
Resul Takak <resultakak@gmail.com>
2021-08-23 01:05:29 +0300

    Update LoginPage.jsx

--- client/src/components/Pages/LoginPage.jsx
+++ client/src/components/Pages/LoginPage.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import GeneralLayout from "../Layouts/GeneralLayout";
-import Container from "../core/Container";
-import Hero from "../core/Hero";
 
 const LoginPage = () => {
   return (
