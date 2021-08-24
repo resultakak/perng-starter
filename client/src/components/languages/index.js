a3c53e8f4d886b960f2eb7a9be50514eec9582cb
Resul Takak <resultakak@gmail.com>
2021-08-24 11:14:55 +0300

    Create index.js

---
+++ client/src/components/languages/index.js
@@ -0,0 +1,8 @@
+export const messages = {
+  SUCCESSFULL: "Successfull!",
+};
+
+export const errorMessages = {
+  PLEASE_TRY_AGAIN: "Please try again.",
+  INVALID_REPASSWORD: "Password does not confirm",
+};
