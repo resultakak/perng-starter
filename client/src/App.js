b6822e728898025e6b0c65b4e07e03ea46fc26d1
Resul Takak <resultakak@gmail.com>
2021-08-24 11:15:06 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import { ProvideAuth, PrivateRoute } from "./components/auth";
 import HomePage from "./components/Pages/HomePage";
 import RegisterPage from "./components/Pages/RegisterPage";
 import LoginPage from "./components/Pages/LoginPage";
+import ProfilePage from "./components/Pages/ProfilePage";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/signup">
-          <RegisterPage/>
-        </Route>
-        <Route path="/signin">
-          <LoginPage/>
-        </Route>
-        <Route path="/">
-          <HomePage/>
-        </Route>
-      </Switch>
-    </Router>
+    <ProvideAuth>
+      <Router>
+        <Switch>
+          <Route path="/signup">
+            <RegisterPage/>
+          </Route>
+          <Route path="/signin">
+            <LoginPage/>
+          </Route>
+          <PrivateRoute path="/profile">
+            <ProfilePage/>
+          </PrivateRoute>
+          <Route path="/">
+            <HomePage/>
+          </Route>
+        </Switch>
+      </Router>
+    </ProvideAuth>
   );
 }
 
