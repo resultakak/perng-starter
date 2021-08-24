f56f8d43150f4ffdfb59e269b80c1662343409b6
Resul Takak <resultakak@gmail.com>
2021-08-24 11:14:25 +0300

    Update auth.js

--- server/src/lib/auth.js
+++ server/src/lib/auth.js
@@ -1,15 +1,17 @@
 const jwt = require("jsonwebtoken");
+const { AuthenticationError } = require("apollo-server");
 
 const { JWT_SECRET } = process.env;
+import errors from "./errors";
 
-const getUser = (token) => {
+const getUser = token => {
   try {
     if (token) {
       return jwt.verify(token, JWT_SECRET);
     }
     return null;
-  } catch (error) {
-    return null;
+  } catch (e) {
+    throw new AuthenticationError(errors.SESSI0N_EXPIRED);
   }
 };
 
